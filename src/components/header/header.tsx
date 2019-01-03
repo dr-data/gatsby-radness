e1ed50f0fb5ad5929d5887e72b9cfe47126058ae
Jason Brewer <jason@devslopes.com>
2019-01-03 14:58:00 -0600

    finished header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './header.sass'
+
+const Header = () => (
+  <div className="header-container">
+    <div className="header-text">title<span className="highlight"> I'm Jason Brewer</span>.</div>
+    <br/>
+    <div className="header-text">I'm a full-stack Web Developer</div>
+  </div>
+)
+
+export default Header
