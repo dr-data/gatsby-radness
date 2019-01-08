a14c90a47e92bdc2c3cf8cef876ddce73e2f35f3
Jason Brewer <jason@devslopes.com>
2019-01-08 07:10:20 -0600

    got the form working

--- src/pages/contact/contact-form.tsx
+++ src/pages/contact/contact-form.tsx
@@ -2,24 +2,17 @@ import * as React from 'react'
 
 const ContactForm = () => (
   <>
-  <form name="contact" method="POST" data-netlify="true">
-    <label htmlFor="fname">Name</label>
-    <input type="text" id="fname" name="name" required />
-    <label htmlFor="email">Email</label>
-    <input type="email" id="email" name="email" required />
-    <button type="submit">
-      <span>Send Message</span>
-    </button>
-    <input type="hidden" name="form-name" value="contact" />
-  </form>
-    <form name="contact-form" method="POST" data-netlify="true">
-      <label>Boom</label>
-      <input type="text" placeholder="name" name="name"/>
-      <textarea name='textarea' cols={30} rows={10}/>
-      <button type="submit">Send</button>
-      <input type="hidden" name="form-name" value="contact-form" />
+    <form name="contact" method="POST" data-netlify="true">
+      <label htmlFor="fname">Name</label>
+      <input type="text" id="fname" name="name" required/>
+      <label htmlFor="email">Email</label>
+      <input type="email" id="email" name="email" required/>
+      <button type="submit">
+        <span>Send Message</span>
+      </button>
+      <input type="hidden" name="form-name" value="contact"/>
     </form>
   </>
-);
+)
 
 export default ContactForm
