ccb96908a394ad141931cfd10f98e21d6efcee0b
Jason Brewer <jason@devslopes.com>
2019-01-07 20:41:12 -0600

    g

--- src/pages/contact/contact-form.tsx
+++ src/pages/contact/contact-form.tsx
@@ -3,8 +3,6 @@ import * as React from 'react'
 const ContactForm = () => (
   <>
   <form name="contact" method="POST" data-netlify="true">
-    <div>
-    </div>
     <label htmlFor="fname">Name</label>
     <input type="text" id="fname" name="name" required />
     <label htmlFor="email">Email</label>
@@ -15,8 +13,8 @@ const ContactForm = () => (
     <input type="hidden" name="form-name" value="contact" />
   </form>
     <form name="contact-form" method="POST" data-netlify="true">
-      <label htmlFor='theName'>Boom</label>
-      <input id="theName" type="text" placeholder="name" name="name"/>
+      <label>Boom</label>
+      <input type="text" placeholder="name" name="name"/>
       <button type="submit">Send</button>
       <input type="hidden" name="form-name" value="contact-form" />
     </form>
