c33e9b111f4b37f4514fdb8400af5ab205dfda96
Jason Brewer <jason@devslopes.com>
2019-01-07 20:48:57 -0600

    g

--- src/pages/contact/contact-form.tsx
+++ src/pages/contact/contact-form.tsx
@@ -15,8 +15,9 @@ const ContactForm = () => (
     <form name="contact-form" method="POST" data-netlify="true">
       <label>Boom</label>
       <input type="text" placeholder="name" name="name"/>
+      <textarea name='textarea' cols={30} rows={10}/>
       <button type="submit">Send</button>
-      <input type="hidden" name="form-name" value="germ" />
+      <input type="hidden" name="form-name" value="contact-form" />
     </form>
   </>
 );
