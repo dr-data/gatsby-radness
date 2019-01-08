422bd9f7485bb67bddcd0a00cf4e41b377380557
Jason Brewer <jason@devslopes.com>
2019-01-07 18:22:49 -0600

    more testing

--- src/pages/contact/contact-form.tsx
+++ src/pages/contact/contact-form.tsx
@@ -1,26 +1,122 @@
-import * as React from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
+import 'whatwg-fetch' // Fetch Polyfill
 
-export const ContactForm = () => (
-  <form name="contact" method="POST" data-netlify="true">
-    <p>
-      <label>Your Name: <input type="text" name="name" /></label>
-    </p>
-    <p>
-      <label>Your Email: <input type="email" name="email" /></label>
-    </p>
-    <p>
-      <label>Your Role: <select name="role[]" multiple>
-        <option value="leader">Leader</option>
-        <option value="follower">Follower</option>
-      </select></label>
-    </p>
-    <p>
-      <label>Message: <textarea name="message"/></label>
-    </p>
-    <p>
-      <button type="submit">Send</button>
-    </p>
-  </form>
-)
+/*
+  ⚠️ This is an example of a contact form powered with Netlify form handling.
+  Be sure to review the Netlify documentation for more information:
+  https://www.netlify.com/docs/form-handling/
+*/
+
+
+const encode = data => {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&')
+}
+
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      message: '',
+      showModal: false,
+    }
+  }
+
+  handleInputChange = event => {
+    const target = event.target
+    const value = target.value
+    const name = target.name
+    this.setState({
+      [name]: value,
+    })
+  }
+
+  handleSubmit = event => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(this.handleSuccess)
+      .catch(error => alert(error))
+    event.preventDefault()
+  }
+
+  handleSuccess = () => {
+    this.setState({
+      name: '',
+      email: '',
+      message: '',
+      showModal: true,
+    })
+  }
+
+  closeModal = () => {
+    this.setState({ showModal: false })
+  }
+
+  render() {
+    return (
+      <form
+        name="contact"
+        onSubmit={this.handleSubmit}
+        data-netlify="true"
+        data-netlify-honeypot="bot"
+        overlay={this.state.showModal}
+        onClick={this.closeModal}
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label>
+            Don’t fill this out:{' '}
+            <input name="bot" onChange={this.handleInputChange} />
+          </label>
+        </p>
+
+        <input
+          name="name"
+          type="text"
+          placeholder="Full Name"
+          value={this.state.name}
+          onChange={this.handleInputChange}
+          required
+        />
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          value={this.state.email}
+          onChange={this.handleInputChange}
+          required
+        />
+        <textarea
+          name="message"
+          type="text"
+          placeholder="Message"
+          value={this.state.message}
+          onChange={this.handleInputChange}
+          required
+        />
+        <button name="submit" type="submit" value="Send" />
+
+        <div visible={this.state.showModal}>
+          <p>
+            Thank you for reaching out. I will get back to you as soon as
+            possible.
+          </p>
+          <button onClick={this.closeModal}>Okay</button>
+        </div>
+      </form>
+    )
+  }
+}
+
+ContactForm.propTypes = {
+  data: PropTypes.object,
+}
 
 export default ContactForm
