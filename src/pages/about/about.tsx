c52f18cd89d3e3c9c01b4f046d1646c377befe4b
Jason Brewer <jason@devslopes.com>
2019-01-09 14:08:40 -0600

    removed about page padding

--- src/pages/about/about.tsx
+++ src/pages/about/about.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import './about.sass'
 import PageHeader from '../../components/page-header/page-header'
 import BulletWrap from '../../components/bullet-wrap/bullet-wrap'
 import ProfileContainer from './profile-container/profile-container'
