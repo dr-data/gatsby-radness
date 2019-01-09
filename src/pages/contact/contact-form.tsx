32719c02df8ae2ac827f7570eef9c55d79e5601f
Jason Brewer <jason@devslopes.com>
2019-01-09 08:54:49 -0600

    another form test

--- src/pages/contact/contact-form.tsx
+++ src/pages/contact/contact-form.tsx
@@ -67,6 +67,16 @@ class ContactForm extends React.Component<any, any> {
               <input type="hidden" name="form-name" value="contact"/>
               <button type="submit">$ git push origin master (click to send)</button>
             </form>
+            <form name="contact" method="POST" data-netlify="true">
+              <label htmlFor="fname">Name</label>
+              <input type="text" id="fname" name="name" required/>
+              <label htmlFor="email">Email</label>
+              <input type="email" id="email" name="email" required/>
+              <button type="submit">
+                <span>Send Message</span>
+              </button>
+              <input type="hidden" name="form-name" value="contact"/>
+            </form>
           </div>
         </div>
       </div>
