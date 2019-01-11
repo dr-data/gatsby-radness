c581b6eb7969942ea5ddb492bd6776f08d6d8f5a
Jason Brewer <jason@devslopes.com>
2019-01-11 11:49:47 -0600

    add container to project template

--- src/components/project-template/project-template.tsx
+++ src/components/project-template/project-template.tsx
@@ -20,46 +20,48 @@ class ProjectTemplate extends React.Component<any, any> {
 
     return (
       <Layout>
-        <div className='hideOnMobile'>
-          <ProjectHeader title={headerTitle}/>
-        </div>
-        <div className='template-container'>
-          <div className="template-content">
-            <Link className="back-link" to="projects"><i className="fas fa-chevron-circle-left"/>
-              Back to projects
-            </Link>
-            <img className="img-project-view" src={url} alt=''/>
-            <div className="source-code">
-              {sourceCode !== null &&
-              <a className="git-btn" href={sourceCode} target="_blank">
-                <i className="fab fa-github"/>Source Code
-              </a>}
-            </div>
-            <h2 className="template-title">Project Brief</h2>
-            <hr/>
-            {<div className="template-text" dangerouslySetInnerHTML={{ __html: html }}/>}
+        <div className='container'>
+          <div className='hideOnMobile'>
+            <ProjectHeader title={headerTitle}/>
           </div>
-          <div className='template-sidebar'>
-            <div className='template-sidebar-box'>
-              <h3 className="template-title">{projectTitle}</h3>
-              <div className="template-text">{summary}</div>
-              <div className='btn-wrapper'>
-                <a className="template-web-link" href={website} target="_blank">View website
-                  <i className="fas fa-arrow-circle-right"/>
-                </a>
+          <div className='template-container'>
+            <div className="template-content">
+              <Link className="back-link" to="projects"><i className="fas fa-chevron-circle-left"/>
+                Back to projects
+              </Link>
+              <img className="img-project-view" src={url} alt=''/>
+              <div className="source-code">
+                {sourceCode !== null &&
+                <a className="git-btn" href={sourceCode} target="_blank">
+                  <i className="fab fa-github"/>Source Code
+                </a>}
               </div>
-            </div>
-            {skills !== null && <div className='template-sidebar-box'>
-              <h3 className="template-title">Technologies</h3>
+              <h2 className="template-title">Project Brief</h2>
               <hr/>
-              <div className="skill-list-container">
-                {skills.map(skill => (
-                  <ul key={skill} className="skill-ul">
-                    <li className="skill-l">{skill}</li>
-                  </ul>
-                ))}
+              {<div className="template-text" dangerouslySetInnerHTML={{ __html: html }}/>}
+            </div>
+            <div className='template-sidebar'>
+              <div className='template-sidebar-box'>
+                <h3 className="template-title">{projectTitle}</h3>
+                <div className="template-text">{summary}</div>
+                <div className='btn-wrapper'>
+                  <a className="template-web-link" href={website} target="_blank">View website
+                    <i className="fas fa-arrow-circle-right"/>
+                  </a>
+                </div>
               </div>
-            </div>}
+              {skills !== null && <div className='template-sidebar-box'>
+                <h3 className="template-title">Technologies</h3>
+                <hr/>
+                <div className="skill-list-container">
+                  {skills.map(skill => (
+                    <ul key={skill} className="skill-ul">
+                      <li className="skill-l">{skill}</li>
+                    </ul>
+                  ))}
+                </div>
+              </div>}
+            </div>
           </div>
         </div>
       </Layout>
